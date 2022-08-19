aba03afd51557958827b10ee481557d1c3694d88
SShipon <nxshipon@gmail.com>
2022-08-19 18:03:37 +0600

    if else code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let names =['shipon', 'Jhankar', 'Mir hossian', 'kamal']
 let index = -1
 let name =names[index++];
-
+                                        
 
 setInterval(() => {
    
@@ -10,7 +10,75 @@ setInterval(() => {
     if (index == names.length) {
         index = 0;
     }
-},1000)
+},2000)
+
+
+
+let myName = ['shipon', 'jams', 'lemon', 'limes', 'tom clem']
+
+let index1 = -1
+
+let name1 = myName[index1++]
+
+setInterval(() => {
+    name1 = myName[index1++]
+    console.log(name1, name1.length);
+    if (index1 == myName.length) {
+        index1 = 0;
+    }
+}, 1000)
+
+
+const number = [1, 2, 3, 7, 9, 6,]
+
+console.log(number);
+let names =['shipon', 'Jhankar', 'Mir hossian', 'kamal']
+let index = -1
+let name =names[index++];
+                                        
+
+setInterval(() => {
+   
+    name = names[index++];
+    console.log(name, name.length);
+    if (index == names.length) {
+        index = 0;
+    }
+},2000)
+
+
+
+let myName = ['shipon', 'jams', 'lemon', 'limes', 'tom clem']
+
+let index1 = -1
+
+let name1 = myName[index1++]
+
+setInterval(() => {
+    name1 = myName[index1++]
+    console.log(name1, name1.length);
+    if (index1 == myName.length) {
+        index1 = 0;
+    }
+}, 1000)
+
+
+const number = [1, 2, 3, 7, 9, 6,]
+
+console.log(number);
+let names =['shipon', 'Jhankar', 'Mir hossian', 'kamal']
+let index = -1
+let name =names[index++];
+                                        
+
+setInterval(() => {
+   
+    name = names[index++];
+    console.log(name, name.length);
+    if (index == names.length) {
+        index = 0;
+    }
+},2000)
 
 
 
